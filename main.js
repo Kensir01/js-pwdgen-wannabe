04ee59eff64383cd797cef0de00b8671a40ca1ce
Demetrio Turchetta <Kensir96@gmail.com>
2022-01-19 16:01:30 +0100

    Aggiunto codice js

---
+++ main.js
@@ -0,0 +1,8 @@
+
+let nome = prompt(`Inserisci il tuo nome`)
+
+let cognome = prompt(`Inserisci il tuo cognome`)
+
+let colore = prompt(`Inserisci il tuo colore preferito`)
+
+document.getElementById(`password`).innerHTML = nome + cognome + colore + `21`
